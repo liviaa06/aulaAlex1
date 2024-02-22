fc0c60df63d7751df4a26d3c71b5eebeaeabc386
Livia <128159882+liviaa06@users.noreply.github.com>
2024-02-22 09:50:12 -0300

    Add files via upload

---
+++ typestring.js
@@ -0,0 +1,13 @@
+const texto1 = "Olá, mundo!";
+const texto2 = "Olá, mundo!";
+const senha = "senhaSuperSegura456!";
+const StringDeNumeros = "34567";
+
+const citacao = "Meu nome é ";
+const seuNome = "Livia"
+//console.log(citacao)
+
+//concatenação
+
+console.log(citacao + seuNome)
+
