4f6ed9bf0e056fd163eb5227f94648d9ef9bbc96
Livia <128159882+liviaa06@users.noreply.github.com>
2024-02-23 08:20:32 -0300

    Add files via upload

---
+++ typer-boolean.js
@@ -0,0 +1,11 @@
+const primeiroNumero = 5;
+const segundoNumero = 10;
+
+//console.log(primeiroNumero === segundoNumero);
+//console.log(primeiroNumero > segundoNumero);
+//console.log(primeiroNumero < segundoNumero);
+
+const texto1 = "Alura";
+const texto2 = "a";
+
+console.log(texto1 === texto2);
